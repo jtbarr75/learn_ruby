9e9e084eac283145e5eb0e15a3cf1fe3e7895a4a
Jason Barr <Barr75@gmail.com>
2020-01-22 10:52:32 -0800

    completed exercises

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
 class Timer
-  #write your code here
+  
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def time_string
+    s = @seconds % 60
+    m = (@seconds / 60) % 60
+    h = @seconds / (60 * 60)
+    format("%02d:%02d:%02d", h, m, s)
+  end
+
 end
